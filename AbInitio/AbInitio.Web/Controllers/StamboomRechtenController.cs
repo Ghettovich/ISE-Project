31fd845b62fa8708e712afb1c7ae528d2c7c2d91
okanok <okanok94@gmail.com>
2016-05-31 15:06:03 +0200

    UC 6 stamboom rechten beheren

---
+++ AbInitio/AbInitio.Web/Controllers/StamboomRechtenController.cs
@@ -0,0 +1,91 @@
+﻿using AbInitio.Web.DAL;
+using AbInitio.Web.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbInitio.Web.Controllers
+{
+    public class StamboomRechtenController : Controller
+    {
+        StamboomRechtenDAL rechtenDAL = new StamboomRechtenDAL();
+        // GET: StamboomRechten
+        public ActionResult Index()
+        {
+            Session["stamboom"] = 1;
+            if (Session["stamboom"] == null)
+            {
+                return Redirect("/Home");
+            }
+            List<AccountModel> accounts = rechtenDAL.getGerechtigden((int)Session["stamboom"]);
+
+            return View(accounts);
+        }
+
+        [HttpGet]
+        public ActionResult Toevoegen()
+        {
+            List<AccountModel> account = new List<AccountModel>();
+            return View(account);
+        }
+
+        [HttpPost]
+        public ActionResult Toevoegen(AccountModel model)
+        {
+            Regex reg = new Regex(@"^[a-zA-Z'.]{1,40}$");
+            if (reg.IsMatch(model.gebruikersnaam) == false)
+            {
+                return Redirect("Toevoegen");
+            }
+            List<AccountModel> accounts = new List<AccountModel>();
+            accounts = rechtenDAL.getGebruikers(model.gebruikersnaam, (int)Session["stamboom"]);
+            return View(accounts);
+        }
+
+        [HttpGet]
+        public ActionResult ToevoegenRecht()
+        {
+
+            return Redirect("Index");
+        }
+
+        [HttpPost]
+        public ActionResult ToevoegenRecht(AccountModel model)
+        {
+            rechtenDAL.toevoegenGerechtigde(model.accountId,(int)Session["stamboom"],Request["rechtenText"].ToString());
+            return Redirect("Index");
+        }
+
+        [HttpGet]
+        public ActionResult verwijderenRecht(int stamboomAccountId)
+        {
+            rechtenDAL.verwijderenGerechtigde(stamboomAccountId);
+            return Redirect("Index");
+        }
+
+        [HttpGet]
+        public ActionResult wijzigenRecht(int stamboomAccountId, int recht)
+        {
+            switch (recht)
+            {
+                case 2:
+                    recht = 3;
+                    break;
+                case 3:
+                    recht = 2;
+                    break;   
+            }
+            AccountModel model = new AccountModel
+            {
+                stamboomAccountId = stamboomAccountId,
+                stamboomRechten = recht
+            };
+            rechtenDAL.wijzigenGerechtigde(model.stamboomAccountId, model.stamboomRechten.ToString());
+            return Redirect("Index");
+        }
+    }
+}
---
+++ AbInitio/AbInitio.Web/DAL/StamboomRechtenDAL.cs
@@ -0,0 +1,210 @@
+﻿using AbInitio.Web.App_Start;
+using AbInitio.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace AbInitio.Web.DAL
+{
+    public class StamboomRechtenDAL
+    {
+        public IDataReader reader { get; set; }
+
+        public List<AccountModel> getGerechtigden(int stamboomid)
+        {
+            List <AccountModel> accounts =  new List<AccountModel>();
+            AccountModel account = null;
+            try
+            {
+                using (DataConfig dbdc = new DataConfig())
+                {
+                    dbdc.Open();
+                    using (IDbCommand cmd = dbdc.CreateCommand())
+                    {
+                        cmd.CommandText = "dbo.spd_OverzichtRechten";
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        IDataParameter pm = cmd.CreateParameter();
+                        pm.Direction = ParameterDirection.Input;
+
+                        pm.ParameterName = "@stamboomid";
+                        pm.Value = stamboomid;
+                        cmd.Parameters.Add(pm);
+
+                        using (IDataReader dr = dbdc.CreateSqlReader())
+                        {
+                            while (dr.Read())
+                            {
+                                account = new AccountModel
+                                {
+                                    stamboomAccountId = (int)dr["stamboomAccountid"],
+                                    gebruikersnaam = dr["gebruikersnaam"].ToString(),
+                                    stamboomRechten = (int)dr["stamboomrechten"]
+
+                                };
+                                accounts.Add(account);
+
+                            }
+                        }
+
+                    }
+
+                } return accounts;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<AccountModel> getGebruikers(string gebruikersnaam, int stamboomId)
+        {
+            List <AccountModel> accounts =  new List<AccountModel>();
+            AccountModel account = null;
+            try
+            {
+                using (DataConfig dbdc = new DataConfig())
+                {
+                    dbdc.Open();
+                    using (IDbCommand cmd = dbdc.CreateCommand())
+                    {
+                        cmd.CommandText = "dbo.spd_AccountsZoeken";
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        IDataParameter pm = cmd.CreateParameter();
+                        pm.Direction = ParameterDirection.Input;
+
+                        pm.ParameterName = "@accountNaam";
+                        pm.Value = gebruikersnaam;
+                        cmd.Parameters.Add(pm);
+                        pm = cmd.CreateParameter();
+                        pm.ParameterName = "@stamboomId";
+                        pm.Value = stamboomId;
+                        cmd.Parameters.Add(pm);
+
+                        using (IDataReader dr = dbdc.CreateSqlReader())
+                        {
+                            while (dr.Read())
+                            {
+                                account = new AccountModel
+                                {
+                                    gebruikersnaam = dr["gebruikersnaam"].ToString(),
+                                    accountId = (int)dr["accountid"]
+                                };
+                                accounts.Add(account);
+
+                            }
+                        }
+
+                    }
+
+                } return accounts;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void toevoegenGerechtigde(int accountId, int stamboomId, string rechten)
+        {
+            try
+            {
+                using (DataConfig dbdc = new DataConfig())
+                {
+                    dbdc.Open();
+                    using (IDbCommand cmd = dbdc.CreateCommand())
+                    {
+                        cmd.CommandText = "dbo.spd_ToevoegenRechten";
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        IDataParameter pm = cmd.CreateParameter();
+                        pm.Direction = ParameterDirection.Input;
+
+                        pm.ParameterName = "@stamboomid";
+                        pm.Value = stamboomId;
+                        cmd.Parameters.Add(pm);
+                        pm = cmd.CreateParameter();
+                        pm.ParameterName = "@accountid";
+                        pm.Value = accountId;
+                        cmd.Parameters.Add(pm);
+                        pm = cmd.CreateParameter();
+                        pm.ParameterName = "@recht";
+                        pm.Value = rechten;
+                        cmd.Parameters.Add(pm);
+
+                        cmd.ExecuteReader();
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void verwijderenGerechtigde(int stamboomAccountId)
+        {
+            try
+            {
+                using (DataConfig dbdc = new DataConfig())
+                {
+                    dbdc.Open();
+                    using (IDbCommand cmd = dbdc.CreateCommand())
+                    {
+                        cmd.CommandText = "dbo.spd_VerwijderenRechten";
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        IDataParameter pm = cmd.CreateParameter();
+                        pm.Direction = ParameterDirection.Input;
+
+                        pm.ParameterName = "@stamboomAccountId";
+                        pm.Value = stamboomAccountId;
+                        cmd.Parameters.Add(pm);
+
+                        cmd.ExecuteReader();
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void wijzigenGerechtigde(int stamboomAccountId, string rechten)
+        {
+            try
+            {
+                using (DataConfig dbdc = new DataConfig())
+                {
+                    dbdc.Open();
+                    using (IDbCommand cmd = dbdc.CreateCommand())
+                    {
+                        cmd.CommandText = "dbo.spd_WijzigenRechten";
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        IDataParameter pm = cmd.CreateParameter();
+                        pm.Direction = ParameterDirection.Input;
+
+                        pm.ParameterName = "@stamboomAccountId";
+                        pm.Value = stamboomAccountId;
+                        cmd.Parameters.Add(pm);
+                        pm = cmd.CreateParameter();
+                        pm.ParameterName = "@recht";
+                        pm.Value = rechten;
+                        cmd.Parameters.Add(pm);
+
+                        cmd.ExecuteReader();
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
