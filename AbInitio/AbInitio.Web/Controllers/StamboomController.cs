4a0e55f7f1d476858d7fe464b3d860a59ff1be58
koen <koen967@hotmail.com>
2016-06-06 16:05:57 +0200

    Revert "WijzigStamboom doet het weer"
    This reverts commit 521807d46838cf5ad14350ff8ee2e8f0e7ac8121.

--- AbInitio/AbInitio.Web/Controllers/StamboomController.cs
+++ AbInitio/AbInitio.Web/Controllers/StamboomController.cs
@@ -140,8 +140,8 @@ namespace AbInitio.Web.Controllers
             return Redirect("OverzichtStambomen");
         }
 
-        [HttpPost]
-        public ActionResult GetWijzigStamboom(int stamboomid)
+        [HttpGet]
+        public ActionResult WijzigStamboom(int stamboomid)
         {
             StamboomViewModel model = new StamboomViewModel();
             model.stamboomid = stamboomid;
